60483b31410cea8d5aa694fa2f3c2b0659343a17
Aaron Johnston <aaron.johnston@me.com>
2021-11-08 08:13:19 -0500

    divide

--- Proj4/TomasuloGUISkeleton-2019/src/tomasulogui/IntDivide.java
+++ Proj4/TomasuloGUISkeleton-2019/src/tomasulogui/IntDivide.java
@@ -3,17 +3,24 @@ package tomasulogui;
 public class IntDivide extends FunctionalUnit {
 
     public static final int EXEC_CYCLES = 7;
-
+    public int doneFlag = 0;
     public IntDivide(PipelineSimulator sim) {
         super(sim);
     }
 
     public int calculateResult(int station) {
         
-    	int operand1 = FunctionalUnit.stations[station].data1
-    	int operand2 = FunctionalUnit.stations[station].data2
+    	int operand1 = FunctionalUnit.stations[station].getData1();
+    	int operand2 = FunctionalUnit.stations[station].getData2();
         int result = operand1/operand2;
-        return result;
+    	if (doneFlag == getExecCycles()) {
+    		doneFlag = 0;
+    		return result;
+    	}
+    	else {
+    		doneFlag += 1;
+    		return -1;
+    	}
     }
 
     public int getExecCycles() {
