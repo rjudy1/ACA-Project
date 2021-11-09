99f0746b70547b9678172f6a4e642cb30af6b80b
Aaron Johnston <aaron.johnston@me.com>
2021-11-09 14:19:12 -0500

    Additions to Mult/Divide FU

--- Proj4/TomasuloGUISkeleton-2019/src/tomasulogui/IntDivide.java
+++ Proj4/TomasuloGUISkeleton-2019/src/tomasulogui/IntDivide.java
@@ -3,24 +3,64 @@ package tomasulogui;
 public class IntDivide extends FunctionalUnit {
 
     public static final int EXEC_CYCLES = 7;
-    public int doneFlag = 0;
+    public int doneFlag = 1;
+    
+    boolean requestWriteback = false;
+    boolean canWriteback = false;
+    
+    public void setRequestWriteback(boolean reqWB) {
+    	requestWriteback = reqWB;
+    }
+
+    public int getRequestWriteBack() {
+        return requestWriteback;
+    }
+    
+    public void setCanWriteback(boolean canWB) {
+    	canWriteback = canWB;
+    }
+    
+    public int getCanWriteback() {
+        return canWriteback;
+    }
+    
+    public void squashAll() {
+    	for (int i = 0; i < 2; i++) {
+    		stations[i] = -1;
+    	}
+     	requestWriteback = false;
+    }
+    
     public IntDivide(PipelineSimulator sim) {
         super(sim);
     }
 
     public int calculateResult(int station) {
-        
-    	int operand1 = FunctionalUnit.stations[station].getData1();
-    	int operand2 = FunctionalUnit.stations[station].getData2();
-        int result = operand1/operand2;
-    	if (doneFlag == getExecCycles()) {
-    		doneFlag = 0;
-    		return result;
-    	}
-    	else {
-    		doneFlag += 1;
-    		return -1;
-    	}
+    	 if (canWriteback) {
+             requestWriteback = false;
+         }
+         if (!requestWriteback) {
+ 	    	if (doneFlag == getExecCycles()) {
+ 	        	int operand1 = stations[station].getData1();
+ 	        	int operand2 = stations[station].getData2();
+ 	        	int result = operand1*operand2;
+ 	    		requestWriteback = True;
+ 	    		doneFlag = 1;
+ 	    	}
+ 	    	else {
+ 	    		doneFlag += 1;
+ 	    		return -1;
+ 	    	}
+         }
+         // check reservationStations for cdb data
+         if (cdb.getDataValid()) {
+             for (int i = 0; i < BUFFER_SIZE; i++) {
+               if (stations[i] != null) {
+                 stations[i].snoop(cdb);
+               }
+             }
+         }
+         
     }
 
     public int getExecCycles() {
--- Proj4/TomasuloGUISkeleton-2019/src/tomasulogui/IntMult.java
+++ Proj4/TomasuloGUISkeleton-2019/src/tomasulogui/IntMult.java
@@ -3,28 +3,71 @@ package tomasulogui;
 public class IntMult extends FunctionalUnit {
 
     public static final int EXEC_CYCLES = 4;
-    public int doneFlag = 0;
+    public int doneFlag = 1;
+    
+    boolean requestWriteback = false;
+    boolean canWriteback = false;
+    
+    public void setRequestWriteback(boolean reqWB) {
+    	requestWriteback = reqWB;
+    }
+
+    public int getRequestWriteBack() {
+        return requestWriteback;
+    }
+    
+    public void setCanWriteback(boolean canWB) {
+    	canWriteback = canWB;
+    }
+    
+    public int getCanWriteback() {
+        return canWriteback;
+    }
+    
     public IntMult(PipelineSimulator sim) {
         super(sim);
     }
 
-    public int calculateResult(int station) {
-    	
-    	// Do tags get resolved here or earlier?
-    	int operand1 = FunctionalUnit.stations[station].getData1();
-    	int operand2 = FunctionalUnit.stations[station].getData2();
-        int result = operand1*operand2;
-        if (doneFlag == getExecCycles()) {
-    		doneFlag = 0;
-    		return result;
+    public void squashAll() {
+    	for (int i = 0; i < 2; i++) {
+    		stations[i] = -1;
     	}
-    	else {
-    		doneFlag += 1;
-    		return -1;
-    	}
+     	requestWriteback = false;
     }
-
+    
     public int getExecCycles() {
         return EXEC_CYCLES;
     }
+
+    public int calculateResult(int station) {        
+        if (canWriteback) {
+            requestWriteback = false;
+        }
+        if (!requestWriteback) {
+	    	if (doneFlag == getExecCycles()) {
+	        	int operand1 = stations[station].getData1();
+	        	int operand2 = stations[station].getData2();
+	        	int result = operand1*operand2;
+	    		requestWriteback = True;
+	    		doneFlag = 1;
+	    	}
+	    	else {
+	    		doneFlag += 1;
+	    		return -1;
+	    	}
+        }
+        // check reservationStations for cdb data
+        if (cdb.getDataValid()) {
+            for (int i = 0; i < BUFFER_SIZE; i++) {
+              if (stations[i] != null) {
+                stations[i].snoop(cdb);
+              }
+            }
+        }
+        
+        
+        
+    }
+
+    
 }
