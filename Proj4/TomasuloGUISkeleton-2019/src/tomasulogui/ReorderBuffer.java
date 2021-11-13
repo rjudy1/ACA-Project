2eb865716aa065384847041c4d4b95088e49d026
Rachael Judy <rljudy74981@icloud.com>
2021-11-13 15:00:13 -0500

    check done

--- Proj4/TomasuloGUISkeleton-2019/src/tomasulogui/ReorderBuffer.java
+++ Proj4/TomasuloGUISkeleton-2019/src/tomasulogui/ReorderBuffer.java
@@ -43,7 +43,7 @@ public class ReorderBuffer {
       return false;
     }
 
-    if (retiree.isHaltOpcode()) {
+    if (retiree.isHaltOpcode() && retiree.isComplete()) {
       halted = true;
       return true;
     }
@@ -52,7 +52,7 @@ public class ReorderBuffer {
     // TODO - this is where you look at the type of instruction and
     // figure out how to retire it properly
     // case statement
-    if (buff[0].opcode != null) {
+    if (buff[0].opcode != null && retiree.isComplete()) {
 	    switch ((IssuedInst.INST_TYPE)buff[0].opcode) {
 	    case ADD:
 	    case ADDI:
